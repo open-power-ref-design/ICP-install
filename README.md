265de8ed3de4a5af479eb8f4dbeb7f517a79896b
Mick Tarsel <mtarsel@gmail.com>
2018-10-04 16:13:59 -0700

    pulled from my repo
    taken from my repo at https://github.com/mtarsel/ICP-POWER-Up

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# ICP-install
-Install ICP utilizing POWER-Up
+# ICP-POWER-Up
+Install ICP via POWER-Up
+
+# Instructions
+
+Follow instructions on POWER-Up repo: https://github.com/open-power-ref-design-toolkit/power-up
+
+Provide a public IP address to our master node in order to access the ICP dashboard.
+
+Launch your cluster with ```pup deploy configuration.yml``` making sure your configuration.yml includes the snippet from the [icp-config.yaml](./icp-config.yaml). 
+
+NOTE: You will have to enter your sudo password once the deploy is started and then hit Enter a few times. This process will take over 1 hour to complete.
+
+This will run through the entire installation process, starting with the installation of some Linux distribution and finishing by running the [icp-install.sh](./icp-install.sh) and installing ICP. Make sure your configuration file contains the info from [icp-config.yaml](./icp-config.yaml) in this repo.
+
+
+Deploy only software on nodes: ```pup post-deploy configuration.yml```
+
+
+# How it Works
+The YAML file in this repo is only a snippet used to deploy a cluster. Add the [icp-config.yaml](./icp-config.yaml) section in this repo to the bottom of your actual config script.
+
+POWER-Up will identify a single node via the hostname, for our example the hostname is server-1.
+
+This single node will become the ICP Master. The bash script in this repo will be executed on that single ICP Master node.
+
+
+# Installing ICP
+
+ICP is installed via [icp-install.sh](./icp-install.sh) in this repo. It utilizes 2 files taken from other repos.
+
+See: https://github.com/Unicamp-OpenPower/docker_on_power
+
+and
+
+https://github.com/rpsene/icp-scripts
+
+# Removing and Cleanup
+
+Remove your cluster ```teardown switches --data configuration.yml; teardown deployer --container configuration.yml```
---
+++ icp-install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#Written by Rafael Sene and Mick Tarsel
+
+# Trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "Bye!"
+}
+
+PROJECT_DIR=$(pwd)
+
+# ICP Variables
+ICP_FILE=ibm-cloud-private-ppc64le-2.1.0.3.tar.gz
+ICP_FILE_URL=http://pokgsa.ibm.com/projects/i/icp-2.1.0.3/$ICP_FILE
+ICP_LOCATION=/opt/ibm-cloud-private-2.1.0.3
+INCEPTION=ibmcom/icp-inception:2.1.0.3-ee
+
+# Get the main IP of the host
+HOSTNAME_IP=$(ip route get 1 | awk '{print $NF;exit}')
+HOSTNAME=$(hostname)
+
+if [ -z "$1" ]; then
+    EXTERNAL_IP=$HOSTNAME_IP
+else
+    EXTERNAL_IP=$1
+fi
+
+# Create SSH Key and overwrite any already created
+yes y | ssh-keygen -t rsa -f /root/.ssh/id_rsa -q -P ""
+
+# Append ssh-key in the authorized_keys
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+
+# Updating, upgrading and installing some packages
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -yq
+apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -yq
+apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -yq vim python git
+
+# Verify file is executable and install Docker
+# https://github.com/rpsene/icp-scripts
+[ -x $PROJECT_DIR/install_docker.sh ] || chmod +x install_docker.sh
+$PROJECT_DIR/install_docker.sh
+
+# Configuring ICP details (as described in the documentation)
+sysctl -w vm.max_map_count=262144
+echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf
+sysctl -w net.ipv4.ip_local_port_range="10240  60999"
+echo 'net.ipv4.ip_local_port_range="10240 60999"' | sudo tee -a /etc/sysctl.conf
+
+# Verify what ports are available
+# https://github.com/rpsene/icp-scripts
+[ -x $PROJECT_DIR/check_ports.py ] || chmod +x check_ports.py
+python $PROJECT_DIR/check_ports.py
+
+# Configure network (/etc/hosts)
+# This line is required for a OpenStack or PowerVC environment
+sed -i -- 's/manage_etc_hosts: true/manage_etc_hosts: false/g' /etc/cloud/cloud.cfg
+sed -i '/127.0.1.1/s/^/#/g' /etc/hosts
+sed -i '/ip6-localhost/s/^/#/g' /etc/hosts
+echo -e "$HOSTNAME_IP $HOSTNAME" | tee -a /etc/hosts
+
+# Disable StrictHostKeyChecking ask
+sed -i -- 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+
+# Prepare environment for ICP installation
+wget $ICP_FILE_URL
+tar xf $ICP_FILE -O | docker load
+mkdir -p $ICP_LOCATION && cd $ICP_LOCATION || exit
+docker run -v "$(pwd)":/data -e LICENSE=accept $INCEPTION cp -r cluster /data
+cp /root/.ssh/id_rsa ./cluster/ssh_key
+mkdir -p ./cluster/images
+mv /root/$ICP_FILE ./cluster/images/
+
+cd ./cluster || exit
+
+# Remove the content of the hosts file
+> ./hosts
+
+# Add the IP of the single node in the hosts file
+echo "
+[master]
+$HOSTNAME_IP
+
+[worker]
+$HOSTNAME_IP
+
+[proxy]
+$HOSTNAME_IP
+
+#[management]
+#4.4.4.4
+
+#[va]
+#5.5.5.5
+" >> ./hosts
+
+# Replace the entries in the config file to remove the comments of the external IPs
+sed -i -- "s/# cluster_lb_address: none/cluster_lb_address: $EXTERNAL_IP/g" ./config.yaml
+sed -i -- "s/# proxy_lb_address: none/proxy_lb_address: $EXTERNAL_IP/g" ./config.yaml
+
+# Install ICP
+docker run --net=host -t -e LICENSE=accept -v "$(pwd)":/installer/cluster $INCEPTION install
